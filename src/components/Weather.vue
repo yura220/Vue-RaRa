e7a1c75902f2cbbedf092e6be2ab11d1f52f5a76
yura220 <ruddbfk7@gmail.com>
2025-05-09 17:40:44 +0900

    날씨 페이지 통합 및 컴포넌트 구조 리팩토링

---
+++ src/components/Weather.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import ActivityList from './ActivityList.vue';
+import CodyList from './CodyList.vue';
+import ItemList from './ItemList.vue';
+import { weatherMap } from '../data/weatherDataMap.js';
+
+const route = useRoute();
+const weatherType = route.params.type;
+const weatherData = weatherMap[weatherType] || weatherMap.sunny;
+</script>
+
+<template>
+  <div :class="`weather-page ${weatherType}`">
+    <h1>{{ weatherType.toUpperCase() }} WEATHER</h1>
+    <CodyList :items="weatherData.cody" />
+    <ActivityList :items="weatherData.activities" />
+    <ItemList :items="weatherData.items" />
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../src/components/Home.vue';
-import Sunny from '../src/components/Sunny.vue';
-import Cloudy from '../src/components/Cloudy.vue';
-import Snowy from '../src/components/Snowy.vue';
-import Rainy from '../src/components/Rainy.vue';
+import Weather from '../src/components/Weather.vue';
 import Event from '../src/components/Event.vue';
 
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/Sunny', component: Sunny },
-  { path: '/Cloudy', component: Cloudy },
-  { path: '/Snowy', component: Snowy },
-  { path: '/Rainy', component: Rainy },
-  { path: '/Event', component: Event },
+  { path: '/weather/:type', component: Weather },
+  { path: '/event', component: Event },
 ];
 
 const router = createRouter({
