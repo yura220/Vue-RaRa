085d09df556402bde1f7a5a1d1f149391e509023
yura220 <ruddbfk7@gmail.com>
2025-05-13 17:58:22 +0900

    5/13 수정

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <script setup>
-import { useRoute } from 'vue-router'
-import { ref, onMounted, onBeforeUnmount } from 'vue'
 import Sidebar from './components/Sidebar.vue'
 import HeaderNav from './components/HeaderNav.vue'
 import BottomButton from './components/BottomButton.vue'
-
-const route = useRoute()
-const showSidebar = ref(true)
-
-function handleScroll() {
-  if (route.path === '/') {
-    // 스크롤이 800px 이상 내려가면 사이드바 표시
-    showSidebar.value = window.scrollY > 800
-  } else {
-    showSidebar.value = true
-  }
-}
-
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll)
-  handleScroll()
-})
-
-onBeforeUnmount(() => {
-  window.removeEventListener('scroll', handleScroll)
-})
 </script>
 
 <template>
-  <Sidebar v-if="showSidebar" />
+  <Sidebar />
   <HeaderNav />
   <router-view :key="$route.fullPath" />
   <BottomButton />
 </template>
+
