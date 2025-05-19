974550781e421f4231aea7940c5918ec06a24a4a
byeol01 <https://byeol01.github.io/pf1>
2025-05-19 16:57:40 +0900

    img

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Sidebar from './components/Sidebar.vue'
+import HeaderNav from './components/HeaderNav.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Sidebar />
+  <HeaderNav />
+  <router-view :key="$route.fullPath" />
+  <Footer></Footer>
+</template>
+
