937662c90e15cfc03519fd2b9006dfa4b72ff746
yura220 <ruddbfk7@gmail.com>
2025-05-19 16:27:54 +0900

    바텀버튼 수정

--- src/components/BottomButton.vue
+++ src/components/BottomButton.vue
@@ -5,26 +5,32 @@ import './css/bottomButton.css'
 <template>
   <aside id="aside">
     <div class="aside-box">
-      <router-link to="/sunny">
+
         <button>
-          <i class="a-icon">맑음</i>
+          <router-link to="/sunny">
+            <i class="a-icon">맑음</i>
+          </router-link>
         </button>
-      </router-link>
-      <router-link to="/cloudy">
+
       <button>
-        <i class="a-icon">흐림</i>
+        <router-link to="/cloudy">
+          <i class="a-icon">흐림</i>
+        </router-link>
       </button>
-      </router-link>
-      <router-link to="/rainy">
+
         <button>
-          <i class="a-icon">비</i>
+          <router-link to="/rainy">
+            <i class="a-icon">비</i>
+          </router-link>
         </button>
-      </router-link>
-      <router-link to="/snowy">
+
+
         <button>
-          <i class="a-icon">눈</i>
+          <router-link to="/snowy">
+            <i class="a-icon">눈</i>
+          </router-link>
         </button>
-      </router-link>
+
     </div>
   </aside>
 </template>
