8837401728cc5fec3f908f85c4dd67d02bef3ef2
yura220 <ruddbfk7@gmail.com>
2025-05-19 10:07:52 +0900

    5/16 수정

---
+++ src/components/Event.vue
@@ -0,0 +1,13 @@
+<script setup>
+import './css/event.css'
+</script>
+
+<template>
+  <section id="event">
+    <div class="wrap">
+      <h1>이벤트 페이지 준비 중입니다 🛠</h1>
+    </div>
+  </section>
+</template>
+
+
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,15 @@
+<script setup>
+import './css/header.css'
+</script>
+
+<template>
+  <header id="header">
+    <div class="wrap flex">
+      <div class="nav-right">
+        <img src="https://placehold.co/40x40" alt="유저 아이콘" class="icon" />
+        <img src="https://placehold.co/40x40" alt="메뉴 아이콘" class="icon" />
+        <img src="https://placehold.co/40x40" alt="햄버거" class="icon">
+      </div>
+    </div>
+  </header>
+</template>
