3f3f7e5fa839ecf540609b78a6e4f4dd927d3a82
rhddbrhdfyd <jjklqwe16@gmail.com>
2025-05-20 14:15:10 +0900

    처음 커밋

---
+++ src/components/Event.vue
@@ -0,0 +1,31 @@
+<script setup>
+import './css/event.css'
+import Scratch from './Scratch.vue';
+</script>
+
+<template>
+  <div class="event-container">
+    <section class="event-slide">
+      <img src="/img/event-page.png" alt="이벤트 페이지" />
+      <Scratch />
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.event-container {
+  width: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
+  position: relative;
+}
+
+.event-slide {
+  width: 100%;
+  height: 100%;
+}
+
+.event-slide img {
+  width: 100%;
+}
+</style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,15 @@
+<script setup>
+import './css/header.css'
+</script>
+
+<template>
+  <header id="header">
+    <div class="wrap flex">
+      <div class="nav-right">
+        <img src="/img/my.png" alt="유저 아이콘" class="icon" />
+        <img src="/img/bag.png" alt="장바구니" class="icon" />
+        <img src="/img/hamburger.png" alt="햄버거 아이콘" class="icon">
+      </div>
+    </div>
+  </header>
+</template>
