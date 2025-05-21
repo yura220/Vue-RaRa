c2078c9e8fb515e81491c7041e5120be0ef25189
hiamjkim <hiamjkim@gmail.com>
2025-05-21 12:15:03 +0900

    수정0521

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,25 @@
 <script>
 import './css/main.css'
+
+export default {
+  data() {
+    return {
+      // public/img 폴더에 있는 이미지는 절대 경로로 참조
+      footerImage: '/img/event-logo.png' // 기본 이미지 설정
+    }
+  },
+  watch: {
+    // 경로가 바뀔 때마다 footer 이미지 변경
+    $route(to) {
+      if (to.name === 'event') {
+        this.footerImage = '/img/event-logo.png' // 'event' 경로에서 사용할 이미지
+      } else {
+        this.footerImage = '/img/footer-logo.png' // 다른 경로에서 사용할 이미지
+      }
+    }
+  }
+}
+
 </script>
 
 <template>
@@ -7,7 +27,7 @@ import './css/main.css'
     <div class="wrap">
 <!-- 왼쪽 로고 -->
         <div class="footer-left">
-            <img src="/img/footer-logo.png" alt="로고 이미지" class="logo" />
+            <img :src="footerImage" alt="로고 이미지" class="logo" />
         </div>
 
 <!-- 가운데 텍스트 -->
@@ -15,16 +35,16 @@ import './css/main.css'
             <p class="site-name">LARA</p>
             <p>© 2025 Team LARA</p>
             <p>Designed & Developed by 경유라, 김민주, 이주영, 이한별</p>
-            <p>저작권표시</p>
+            <p>Uicons by <a href="https://www.flaticon.com/uicons" target="_blank">Flaticon</a>
+                 · Images by <a href="https://unsplash.com/ko" target="_blank">Unsplash</a>
+                  · Images by <a href="https://www.freepik.com/" target="_blank">Freepik</a>
+            </p>
         </div>
 
 <!-- 오른쪽 아이콘 -->
         <div class="footer-right">
             <a href="https://github.com/" target="_blank">
                 <img src="/img/github.png" alt="GitHub" />
-            </a>
-            <a href="" target="_blank">
-                <img src="/img/figma.png" alt="Figma" />
             </a>
         </div>
     </div>
--- src/components/js/bestDataMap.js
+++ src/components/js/bestDataMap.js
@@ -92,45 +92,45 @@ export const bestMap = {
   3: {
     main: {
       id: 1,
-      brand: 'Mint&Milk',
-      name: '데일리 로퍼',
-      price: '39,000원',
-      desc: '지금 입기 딱 좋은, 컬러별 데일리 아이템',
+      brand: 'Velor',
+      name: '루미나 시티백',
+      price: '89,000원',
+      desc: '현대적인 디자인과 우아한 디테일로, 세련된 여성들을 위한 핸드백',
       colors: [
-        { id: 1, name: 'Yellow', colorCode:'#f5e7c6', image: '/img/Look1.png' },
-        { id: 2, name: 'Pink', colorCode:'#eacbcb', image: '/img/look1-2.png' },
-        { id: 3, name: 'Sky', colorCode:'#c9dfef', image: '/img/look1-3.png' },
-        { id: 4, name: 'Green', colorCode:'#d7e7d0', image: '/img/look1-4.png' }
+        { id: 1, name: 'White', colorCode:'#F5F5F5', image: '/img/best_bag-1.jpg' },
+        { id: 2, image: '/img/best_bag-2.jpg' },
+        { id: 3, image: '/img/best_bag-3.jpg' },
+        // { id: 4, name: 'Green', colorCode:'#d7e7d0', image: '/img/look1-4.png' }
       ],
     },
     related: [
       {
         id: 1,
-        image: '/img/Look2.png',
-        brand: 'Mint&Milk',
-        name: '여름 티셔츠',
-        price: '39,000원'
+        image: '/img/best_bag-4.jpg',
+        brand: 'Nobla',
+        name: '벨로르 인피니티 토트백',
+        price: '239,000원'
       },
       {
         id: 2,
-        image: '/img/Look2.png',
-        brand: 'Mint&Milk',
-        name: '점프수트',
-        price: '39,000원'
+        image: '/img/best_bag-5.jpg',
+        brand: 'Ermer',
+        name: '노블라 그랜드 숄더백',
+        price: '129,000원'
       },
       {
         id: 3,
-        image: '/img/Look2.png',
-        brand: 'Mint&Milk',
-        name: '크로스백',
-        price: '39,000원'
+        image: '/img/best_bag-6.jpg',
+        brand: 'Lumina',
+        name: '에르메르 숄더백',
+        price: '89,000원'
       },
       {
         id: 4,
-        image: '/img/Look2.png',
-        brand: 'Mint&Milk',
-        name: '화이트 힐',
-        price: '39,000원'
+        image: '/img/best_bag-7.jpg',
+        brand: 'Arcarr',
+        name: '아르카르 에센셜 백',
+        price: '189,000원'
       }
     ]
   },
